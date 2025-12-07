b9b052480552e52b8ea434390b41bf56793c903d
theS1LV3R <me@s1lv3r.codes>
2025-12-07 23:48:13 +0100

    Fix robots

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -2,33 +2,9 @@ import type { APIRoute } from 'astro';
 
 const getRobotsTxt = (sitemapURL: URL) => `\
 User-agent: *
-Content-signal: search=yes,ai-train=no
-Sitemap: ${sitemapURL.href}
 Allow: /
 
-User-agent: Amazonbot
-Disallow: /
-
-User-agent: Applebot-Extended
-Disallow: /
-
-User-agent: Bytespider
-Disallow: /
-
-User-agent: CCBot
-Disallow: /
-
-User-agent: ClaudeBot
-Disallow: /
-
-User-agent: Google-Extended
-Disallow: /
-
-User-agent: GPTBot
-Disallow: /
-
-User-agent: meta-externalagent
-Disallow: /
+Sitemap: ${sitemapURL.href}
 `;
 
 export const GET: APIRoute = ({ site }) => {
