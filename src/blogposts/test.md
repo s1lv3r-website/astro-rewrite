522c1d27406b728f0148ddd5e2aae73166e16e47
theS1LV3R <me@s1lv3r.codes>
2025-12-13 17:50:55 +0100

    Remove test posts
    Got enough proper posts to use those as previews by now

--- src/blogposts/test.md
+++
@@ -1,7 +0,0 @@
----
-title: test
-pubDate: 2025-11-15T04:34:00.000+01:00
-updatedDate: 2025-11-15T04:34:00.000+01:00
-showInUI: false
----
-test
--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,7 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
 const blog = defineCollection({
-  loader: glob({ pattern: "*.{md,mdx}", base: "./src/blogposts" }),
+  loader: glob({ pattern: "*.{md,mdx}", base: "./src/_blogposts" }),
   schema: z.object({
     title: z.string(),
     description: z.string().optional(),
