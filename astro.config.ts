63e5db93fb391340170ffc7a911fbc359f1564fd
theS1LV3R <me@s1lv3r.codes>
2025-12-28 00:43:18 +0100

    Add svgo optimizations

--- astro.config.ts
+++ astro.config.ts
@@ -27,7 +27,9 @@ export default defineConfig({
     allowedHosts: true,
   },
   devToolbar: { enabled: false },
-
+  experimental: {
+    svgo: true,
+  },
   markdown: {
     syntaxHighlight: false,
     remarkPlugins: [sectionize],
